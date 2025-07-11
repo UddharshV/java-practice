6ab4de4d1acc95e9d7f5504011dac9cbb84efa5f
Uddharsh Vasili <Uddharsh.v19@gmail.com>
2025-07-11 15:25:31 +0000

    Removed Duplicate Files

--- Unicode_Style.java
+++
@@ -1,7 +0,0 @@
-package practice_pack.basics;
-
-public class Unicode_Style {
-    public static void main(String[] args) {
-        System.out.println("నమస్కారం"); // Java follows unicode
-    }
-}
